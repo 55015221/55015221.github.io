12de9b72fa9f957a8a74a690b4344a4fef74472a
sygz <55015221@qq.com>
2024-05-27 19:01:15 +0800

    55015221.github.io

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   root: process.cwd(),
   plugins: [vue()],
+  build: {
+    outDir: "docs",
+  }
 })
