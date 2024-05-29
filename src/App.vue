2f24897c8b7c60f192b6097ffb36faaf3d16f13b
sygz <55015221@qq.com>
2024-05-29 10:25:59 +0800

    55015221.github.io

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <script setup lang="ts">
 // import enUS from '@arco-design/web-vue/es/locale/lang/en-us';
-import zhCN from '@arco-design/web-vue/es/locale/lang/zh-cn';
+import zhCN from "@arco-design/web-vue/es/locale/lang/zh-cn";
 import dayjs from "dayjs";
-import {ref} from "vue";
-const isWatermark = ref(true)
+import { ref } from "vue";
+const isWatermark = ref(true);
 
 defineOptions({
-  name: "App"
-})
+	name: "App",
+});
 </script>
 
 <template>
-  <a-config-provider size="medium" :locale="zhCN">
-    <a-watermark v-if="isWatermark" :gap="[200, 200]" :alpha="0.8" :content="['55015221.github.io', dayjs().format('YYYY-MM-DD')]">
-      <router-view/>
-    </a-watermark>
-    <router-view v-else/>
-  </a-config-provider>
+	<a-config-provider size="medium" :locale="zhCN">
+		<a-watermark v-if="isWatermark" :gap="[200, 200]" :alpha="0.1" :content="['55015221.github.io', dayjs().format('YYYY-MM-DD')]">
+			<router-view />
+		</a-watermark>
+		<router-view v-else />
+	</a-config-provider>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
