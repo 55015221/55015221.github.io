fc9ae2f2530d766d7f01561f0318f428d827211c
sygz <55015221@qq.com>
2024-06-03 14:35:36 +0800

    55015221.github.io

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,36 +1,35 @@
-import {RouteRecordRaw} from 'vue-router';
+import { RouteRecordRaw } from "vue-router";
 
-const Layout = () => import('@/layout/index.vue');
+const Layout = () => import("@/layout/index.vue");
 
 // 固定路由（默认路由）
 const staticRoutes: Array<RouteRecordRaw> = [
-    {
-        path: '/',
-        name: 'home',
-        component: Layout,
-        redirect: '/index',
-        children: [
-            {
-                path: 'index',
-                name: 'index',
-                component: () => import('@/views/home/index.vue'),
-                meta: {title: '登录'},
-            },
-            {
-                path: 'about',
-                name: 'about',
-                component: () => import('@/views/about/index.vue'),
-                meta: {title: '登录'},
-            },
-            {
-                path: '/:all(.*)*', // 404路由
-                name: 'notFound',
-                component: () => import('@/views/error/index.vue'),
-                meta: {title: '找不到页面', hidden: true},
-            }
-        ]
-    }
+	{
+		path: "/",
+		name: "home",
+		component: Layout,
+		redirect: "/",
+		children: [
+			{
+				path: "/",
+				name: "home",
+				component: () => import("@/views/home/index.vue"),
+				meta: { title: "登录" },
+			},
+			{
+				path: "about",
+				name: "about",
+				component: () => import("@/views/about/index.vue"),
+				meta: { title: "登录" },
+			},
+			{
+				path: "/:all(.*)*", // 404路由
+				name: "notFound",
+				component: () => import("@/views/error/index.vue"),
+				meta: { title: "找不到页面", hidden: true },
+			},
+		],
+	},
 ];
 
-
-export {staticRoutes};
+export { staticRoutes };
