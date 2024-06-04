3df3e0e82999c49ab2f19a5df163647866d5742d
bily <55015221@qq.com>
2024-06-04 22:34:11 +0800

    55015221.github.io

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -5,8 +5,8 @@
 			:sm="{ span: 22, offset: 1 }"
 			:md="{ span: 20, offset: 2 }"
 			:lg="{ span: 20, offset: 2 }"
-			:xl="{ span: 18, offset: 4 }"
-			:xxl="{ span: 18, offset: 4 }"
+			:xl="{ span: 18, offset: 3 }"
+			:xxl="{ span: 18, offset: 3 }"
 		>
 			<slot />
 		</a-col>
--- src/views/home/components/GoodCard.vue
+++ src/views/home/components/GoodCard.vue
@@ -40,27 +40,35 @@ const cardList = reactive([
 <style scoped lang="scss">
 .arco-card:hover {
 	//box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-	box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
-}
-.image {
-	position: relative;
-	background: url("data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22100px%22%20height%3D%22100px%22%20viewBox%3D%220%200%20100%20100%22%20preserveAspectRatio%3D%22xMidYMid%22%3E%3Cpath%20fill%3D%22none%22%20d%3D%22M24.3%2C30C11.4%2C30%2C5%2C43.3%2C5%2C50s6.4%2C20%2C19.3%2C20c19.3%2C0%2C32.1-40%2C51.4-40%20C88.6%2C30%2C95%2C43.3%2C95%2C50s-6.4%2C20-19.3%2C20C56.4%2C70%2C43.6%2C30%2C24.3%2C30z%22%20stroke%3D%22%2356cfe1%22%20stroke-width%3D%222%22%20stroke-dasharray%3D%22205.271142578125%2051.317785644531256%22%3E%3Canimate%20attributeName%3D%22stroke-dashoffset%22%20calcMode%3D%22linear%22%20values%3D%220%3B256.58892822265625%22%20keyTimes%3D%220%3B1%22%20dur%3D%221%22%20begin%3D%220s%22%20repeatCount%3D%22indefinite%22%2F%3E%3C%2Fpath%3E%3C%2Fsvg%3E")
-		center no-repeat;
-	background-size: 20% 20%;
-}
+	box-shadow: rgba(0, 0, 0, 0.1) 0 10px 50px;
 
-.image img {
-	border: 0;
-	vertical-align: middle;
-	max-width: 100%;
-	position: absolute;
-	inset: 0;
-	object-fit: contain;
+	.image {
+		transform: scale(1.4);
+	}
 }
+.arco-card {
+	.image {
+		position: relative;
+		background: url("data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22100px%22%20height%3D%22100px%22%20viewBox%3D%220%200%20100%20100%22%20preserveAspectRatio%3D%22xMidYMid%22%3E%3Cpath%20fill%3D%22none%22%20d%3D%22M24.3%2C30C11.4%2C30%2C5%2C43.3%2C5%2C50s6.4%2C20%2C19.3%2C20c19.3%2C0%2C32.1-40%2C51.4-40%20C88.6%2C30%2C95%2C43.3%2C95%2C50s-6.4%2C20-19.3%2C20C56.4%2C70%2C43.6%2C30%2C24.3%2C30z%22%20stroke%3D%22%2356cfe1%22%20stroke-width%3D%222%22%20stroke-dasharray%3D%22205.271142578125%2051.317785644531256%22%3E%3Canimate%20attributeName%3D%22stroke-dashoffset%22%20calcMode%3D%22linear%22%20values%3D%220%3B256.58892822265625%22%20keyTimes%3D%220%3B1%22%20dur%3D%221%22%20begin%3D%220s%22%20repeatCount%3D%22indefinite%22%2F%3E%3C%2Fpath%3E%3C%2Fsvg%3E")
+			center no-repeat;
+		background-size: 20% 20%;
+		transition: all ease-in-out 0.3s;
+		transform: scale(1.2);
+	}
 
-.image:before {
-	padding-top: 100%;
-	content: "";
-	display: block;
+	img {
+		border: 0;
+		vertical-align: middle;
+		max-width: 100%;
+		position: absolute;
+		inset: 0;
+		object-fit: contain;
+	}
+
+	.image:before {
+		padding-top: 100%;
+		content: "";
+		display: block;
+	}
 }
 </style>
