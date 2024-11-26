d46b1f7949c2d61e37c0b40a1eb289752d7c2d4c
sygz <55015221@qq.com>
2024-11-26 16:48:54 +0800

    dds

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,18 @@
 <template>
-  <article class="card">
-    <div class="labels">
-      <span class="badge text-bg-danger">热门</span>
-      <span class="badge text-bg-success">流行</span>
-    </div>
-    <div class="actions">
-      <button><i class="bi bi-heart"></i></button>
-      <button><i class="bi bi-brightness-high"></i></button>
-    </div>
+  <article class="card shadow-lg">
+    <!-- Media -->
     <figure class="card-media">
+      <!-- Labels -->
+      <div class="labels">
+        <span class="badge text-bg-danger">热门</span>
+        <span class="badge text-bg-success">流行</span>
+      </div>
+
+      <!-- Actions -->
+      <div class="actions">
+        <button><i class="bi bi-heart"></i></button>
+        <button><i class="bi bi-brightness-high"></i></button>
+      </div>
       <swiper :modules="[Navigation, Pagination, EffectFade, A11y, Controller]" :slides-per-view="1" :space-between="20" loop autoplay navigation :pagination="{clickable: true}" @swiper="onSwiper" @slideChange="onSlideChange">
         <swiper-slide v-for="(image, _) in images" :key="image.id">
           <img class="card-img-top" :src="image.url" :alt="image.title" />
@@ -16,6 +20,7 @@
       </swiper>
     </figure>
 
+    <!-- Body -->
     <div class="card-body">
       <div class="subtitle">{{ subtitle }}</div>
       <h3 class="title animate-underline">
@@ -79,26 +84,33 @@ const {title, subtitle, images, description} = toRefs(props.data);
 <style scoped lang="scss">
 article.card {
   position: relative;
-  .labels,
-  .actions {
-    z-index: 100;
-    position: absolute;
-  }
-  .labels {
-    top: 10px;
-    left: 10px;
-  }
-  .actions {
-    display: flex;
-    flex-direction: column;
-    top: 10px;
-    right: 10px;
-  }
+  border: 0;
+  // box-shadow: var(--bs-shadow-lg);
 
   .card-media {
+    background: url("data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22100px%22%20height%3D%22100px%22%20viewBox%3D%220%200%20100%20100%22%20preserveAspectRatio%3D%22xMidYMid%22%3E%3Cpath%20fill%3D%22none%22%20d%3D%22M24.3%2C30C11.4%2C30%2C5%2C43.3%2C5%2C50s6.4%2C20%2C19.3%2C20c19.3%2C0%2C32.1-40%2C51.4-40%20C88.6%2C30%2C95%2C43.3%2C95%2C50s-6.4%2C20-19.3%2C20C56.4%2C70%2C43.6%2C30%2C24.3%2C30z%22%20stroke%3D%22%2356cfe1%22%20stroke-width%3D%222%22%20stroke-dasharray%3D%22205.271142578125%2051.317785644531256%22%3E%3Canimate%20attributeName%3D%22stroke-dashoffset%22%20calcMode%3D%22linear%22%20values%3D%220%3B256.58892822265625%22%20keyTimes%3D%220%3B1%22%20dur%3D%221%22%20begin%3D%220s%22%20repeatCount%3D%22indefinite%22%2F%3E%3C%2Fpath%3E%3C%2Fsvg%3E")
+      0 0 /20% no-repeat;
+    background-position: center;
+    position: relative;
+
+    .labels,
+    .actions {
+      z-index: 100;
+      position: absolute;
+      display: flex;
+      gap: 5px;
+    }
+    .labels {
+      top: 8px;
+      left: 8px;
+    }
+    .actions {
+      flex-direction: column;
+      top: 8px;
+      right: 8px;
+    }
+
     .swiper-slide {
-      background: url("data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22100px%22%20height%3D%22100px%22%20viewBox%3D%220%200%20100%20100%22%20preserveAspectRatio%3D%22xMidYMid%22%3E%3Cpath%20fill%3D%22none%22%20d%3D%22M24.3%2C30C11.4%2C30%2C5%2C43.3%2C5%2C50s6.4%2C20%2C19.3%2C20c19.3%2C0%2C32.1-40%2C51.4-40%20C88.6%2C30%2C95%2C43.3%2C95%2C50s-6.4%2C20-19.3%2C20C56.4%2C70%2C43.6%2C30%2C24.3%2C30z%22%20stroke%3D%22%2356cfe1%22%20stroke-width%3D%222%22%20stroke-dasharray%3D%22205.271142578125%2051.317785644531256%22%3E%3Canimate%20attributeName%3D%22stroke-dashoffset%22%20calcMode%3D%22linear%22%20values%3D%220%3B256.58892822265625%22%20keyTimes%3D%220%3B1%22%20dur%3D%221%22%20begin%3D%220s%22%20repeatCount%3D%22indefinite%22%2F%3E%3C%2Fpath%3E%3C%2Fsvg%3E")
-        0 0 /20% no-repeat;
       &:before {
         padding-top: 75%;
         content: "";
